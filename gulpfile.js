41e8077828eaebcb88d63a66dd4cd86b31546f37
macfly <manuel@webdesignord.fr>
2016-03-11 09:48:14 +0100

    init depot; destroy css ok premailer ok

---
+++ gulpfile.js
@@ -0,0 +1,137 @@
+/*==================================================
+=            npm install gulp --sav-dev            =
+==================================================*/
+// to disable>dest path replace fs
+/*----------  dependance  > package.json > node_modules  ----------*/
+var gulp     = require('gulp'),
+browserSync  = require('browser-sync'),
+slim         = require("gulp-slim"),
+sass         = require('gulp-sass'),
+plumber      = require('gulp-plumber'),
+premailer    = require('gulp-premailer'),
+autoprefixer = require('gulp-autoprefixer'),
+rename       = require('gulp-rename'),
+using        = require('gulp-using'),
+rm           = require('gulp-rimraf'),
+rimraf       = require('rimraf'),
+prettify     = require('gulp-html-prettify'),
+changed      = require('gulp-changed'),
+debug        = require('gulp-debug');
+// imgmin    = require('gulp-imagemin'),
+
+// src & output
+var  src = 'src/';
+/*=================================
+=            task init            =
+=================================*/
+// browser-sync task !attention il faut un index.html obligatoire
+gulp.task('browserSync',function () {
+  browserSync({
+    server: {
+      baseDir: 'render/FR'
+    }
+  })
+})
+
+
+// cp img folder
+gulp.task('img', function() {
+  return gulp.src([src+'**/images/*.{png,jpg,gif}'])
+  // .pipe(npm()) // img optimize
+  .pipe(changed('src/**/images/'))
+  .pipe(gulp.dest('render'))
+  .pipe(using())
+  .on('end',function () {
+    // start slim to render
+    gulp.start('slim');
+  })
+  .pipe(browserSync.reload({stream: true }));
+})
+
+// sass task
+gulp.task('sass', function() {
+  return gulp.src(src+'**/scss/*.scss')
+  .pipe(plumber())
+  .pipe(sass())
+  .pipe(sass({errLogToConsole: true}))
+  .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+  .pipe(rename(function(path) {
+    path.dirname += "/../css";
+  }))
+  .pipe(gulp.dest('render'))
+  .pipe(using())
+  .pipe(browserSync.reload({stream: true }));
+})
+
+// slim task
+gulp.task('slim', function () {
+  var slimEnd = false;
+  return gulp.src([src+'**/slim/*.slim'])
+  .pipe(using())
+  .pipe(slim( {pretty: true, indent: '2' })) // cb // {read:false},
+  .pipe(gulp.dest('render')) // slim folder
+  .pipe(plumber())
+  .pipe(using())
+  .pipe(rename(function(path) {
+    path.dirname += "/../";
+  }))
+  .pipe(gulp.dest('render')) // html folder
+  .pipe(browserSync.reload({
+    stream: true
+  }))
+  .on('end',function () {
+    slimEnd = true;
+    premailergo(slimEnd);
+  })
+});
+// premailer task // si erreur sass > rendu incomplet à gérer
+gulp.task('premailer', function () {
+  gulp.src('render/**/*.html')
+  .pipe(plumber())
+  .pipe(premailer()) //,{read:false}
+  .pipe(prettify({indent_car:'', indent_size: 2}))
+  .pipe(gulp.dest('render'))
+  .on('end',function () {
+    premailEnd = true;
+    console.log('premailerOK: '+premailEnd+' rm render/slim folder ');
+    // rimraf('./render/**/slim',function (err) {console.log("slim destroy");})
+    rimraf('./render/**/css',function (err) {console.log("css destroy");})
+    // gulp.start('rmRenderSlimFolder');
+    // gulp.start('rmRenderCssFolder');
+  })
+  .pipe(debug({title: 'endPremailer:'}))
+});
+// 
+// premailer
+function premailergo (slimEnd) {
+  console.log('slim complete: '+slimEnd);
+  if(slimEnd=true){
+    gulp.start(['premailer'])
+    .on('end', function (argument) {
+      console.log('yeah is done')
+    })
+  }else{
+    console.log('slim pas prêt.......')
+  }
+};
+gulp.task('rmRenderSlimFolder', function (cb) {
+  rimraf('./render/**/slim',function (err) {
+    console.log("all done del slim");
+    return cb(null);
+  });
+});
+gulp.task('rmRenderCssFolder', function (cb) {
+  rimraf('./render/**/css',function (err) {
+    console.log("all done del css");
+    return cb(null);
+  });
+});
+
+// lancement > fonction watch
+gulp.task('dev',['browserSync','img','slim','sass','premailer'], function() {
+  // src+'*.slim', ,src+'**/*.slim' // pas de fichier sur :root
+  gulp.watch([src+'**/images/*.{png,jpg,gif}'],['img']);
+  // gulp.watch([src+'**/slim/*.slim',src+'**/**/*.slim'],['browserSync','slim','img','premailer']);
+  gulp.watch([src+'**/slim/*.slim',src+'**/**/*.slim'],['slim','sass','premailer']);
+  gulp.watch(src+'**/scss/*.scss',['sass','premailer','slim']);
+});
